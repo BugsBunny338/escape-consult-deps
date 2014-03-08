e5036fb5bf85da3e5f36426c186b07288a0b5881
admin <admin@192.168.1.200>
2014-03-08 17:27:43 +0100

    added bootstrap rendering support

--- Form/DateTimePicker.php
+++ Form/DateTimePicker.php
@@ -12,3 +12,4 @@ class DateTimePicker extends DatePicker {
          
     
 }
+?>
--- Form/Form.php
+++ Form/Form.php
@@ -10,6 +10,8 @@ namespace jb\Form;
 
 class Form extends \Nette\Application\UI\Form {
     
+    protected $bootstrapRendering = true;
+    
     public function __construct(\Nette\ComponentModel\IContainer $parent = NULL, $name = NULL) {
         parent::__construct($parent, $name);
         $this->addProtection();
@@ -31,6 +33,44 @@ class Form extends \Nette\Application\UI\Form {
     public function addEmail($name, $label = NULL, $cols = NULL, $maxLength = NULL) {
 	return $this[$name] = new EmailInput($label, $cols, $maxLength);
     }
+    
+    public function setBootstrapRendering($val = true) {
+        $this->bootstrapRendering = $val;
+    }
+    
+    public function render() {
+        if (!$this->bootstrapRendering) {
+            parent::render();
+        }
+        else {
+            $renderer = $this->getRenderer();
+            $renderer->wrappers['controls']['container'] = NULL;
+            $renderer->wrappers['pair']['container'] = 'div class=form-group';
+            $renderer->wrappers['pair']['.error'] = 'has-error';
+            $renderer->wrappers['control']['container'] = 'div class=col-sm-9';
+            $renderer->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
+            $renderer->wrappers['control']['description'] = 'span class=help-block';
+            $renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
+
+            // make form and controls compatible with Twitter Bootstrap
+            $this->getElementPrototype()->class('form-horizontal');
+
+            foreach ($this->getControls() as $control) {
+                    if ($control instanceof \Nette\Forms\Controls\Button) {
+                            $control->setAttribute('class', empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-default');
+                            $usedPrimary = TRUE;
+
+                    } elseif ($control instanceof \Nette\Forms\Controls\TextBase || $control instanceof \Nette\Forms\Controls\SelectBox || $control instanceof \Nette\Forms\Controls\MultiSelectBox) {
+                            $control->setAttribute('class', 'form-control');
+
+                    } elseif ($control instanceof \Nette\Forms\Controls\Checkbox || $control instanceof \Nette\Forms\Controls\CheckboxList || $control instanceof \Nette\Forms\Controls\RadioList) {
+                            $control->getSeparatorPrototype()->setName('div')->class($control->getControlPrototype()->type);
+                    }
+            }
+            parent::render();
+        }
+    }
+    
 }
 
 ?>
